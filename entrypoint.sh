0e4875282052908b5284cc50a449e4a83d56efbc
Sacrament84 <voloshin07@gmail.com>
2021-10-13 13:15:24 +0600

    add deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python /app/django/notejam/manage.py migrate
-python /app/django/notejam/manage.py runserver 0.0.0.0:8000
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
